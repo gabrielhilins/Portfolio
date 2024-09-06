3abc80f0158282a1f4b142d07d53fae61c4bbaed
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2024-09-06 12:35:24 -0300

    adicionando analytics do vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import { register } from 'swiper/element/bundle';
+import { Analytics } from '@vercel/analytics/react';
 import theme from './theme.js'
 import "./App.css"
 import './i18n'
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')).render(
      <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
        <App />
+       <Analytics />
      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>,
